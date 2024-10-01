423373dc63503b7407fbd205ba6e0279d7f7047b
waeeelhajji <waelhajji.contact@gmail.com>
2024-10-01 15:17:01 +0100

    Week1Day2

---
+++ Week1Day2/Algo/Day2Algo.js
@@ -0,0 +1,24 @@
+/**
+ * Given a string and an integer representing how many times the string should
+ * be repeated, create a new string that is the given string repeated that
+ * many times.
+ * @returns {string} The given string repeated the specified amount of times.
+ */
+function stringRepeat(str,repeatAmount) {}
+
+
+
+
+
+
+
+/**************************************************************************/
+
+/**
+ * Combines the two given strings with the smaller string first or the first
+ * string first if the lengths are the same.
+ * @param {string} s1 The first string.
+ * @param {string} s2 The second string.
+ * @returns {string} The given strings combined.
+ */
+function combineSmallerStringFirst(s1, s2) {}
