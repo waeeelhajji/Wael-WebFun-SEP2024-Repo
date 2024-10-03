c737177857c8c9ff4989b4b65c3f4e626aa88347
waeeelhajji <waelhajji.contact@gmail.com>
2024-10-03 08:14:12 +0100

    Week1Day3

---
+++ Week1Day3/Algo/W1D3Algo.js
@@ -0,0 +1,20 @@
+/**
+ * Finds the longest word in the given array of words.
+ * @param {Array<string>} words
+ * @returns {string} The longest word. If there are multiple words with the same
+ *    length, this will be the last word in the array with that length.
+ */
+
+
+var animals = ["cat", "ele", "dog", "sun"]; // Longest word: "sun"
+var fruits = ["apple", "kiwi", "banana", "strawberry"]; // Longest word: "strawberry"
+var objects = ["book", "notebook", "pen", "encyclopedia"]; // Longest word: "encyclopedia"
+
+
+
+function findLongestWord(words) {
+
+// Your Code here
+
+
+}
